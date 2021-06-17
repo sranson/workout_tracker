1749c32dd22e8b4bd840d90efc57eaafbd6b4cf4
Salome Ranson <salomeranson@gmail.com>
2021-06-16 22:13:07 -0400

    added route to render the stats screen

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const workout = require("../models")
 
-// New Exercise route
-// Render exercise.html
+// Route to get 'New Exercise' screen
 router.get("/exercise", (req, res) => {
     res.sendFile('exercise.html', { root: 'public'});
 })
 
+
+// Route to get 'Stats screen'
+router.get("/stats", (req, res) => {
+    res.sendFile('stats.html', { root: 'public'});
+})
 
 
 
