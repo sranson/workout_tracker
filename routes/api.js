e90cb7b3d84f0885e511b6c8632cfb560e794161
Salome Ranson <salomeranson@gmail.com>
2021-06-16 22:09:51 -0400

    added route for new exercise

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const workout = require("../models")
 
-
-
+// New Exercise route
+// Render exercise.html
+router.get("/exercise", (req, res) => {
+    res.sendFile('exercise.html', { root: 'public'});
+})
 
 
 
