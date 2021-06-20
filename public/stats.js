71d8cfd26bea7381c5ff2b262a17c46837ac3595
Salome Ranson <salomeranson@gmail.com>
2021-06-20 15:34:52 -0400

    added logic to calculate total durations and render data via line graph

--- public/stats.js
+++ public/stats.js
@@ -15,9 +15,26 @@ function calculateTotalWeight(data) {
   return totals;
 }
 
+function calculateTotalDuration(data) {
+  const totals = [];
+
+  data.forEach((workout) => {
+    const workoutDurationTotal = workout.exercises.reduce((total, { type, duration }) => {
+      if (type === 'resistance' || type === 'cardio') {
+        return total + duration;
+      }
+      return total;
+    }, 0);
+
+    totals.push(workoutDurationTotal);
+  });
+
+  return totals;
+}
+
 
 function populateChart(data) {
-  const durations = data.map(({ totalDuration }) => totalDuration);
+  const durations = calculateTotalDuration(data);
   const pounds = calculateTotalWeight(data);
 
   const line = document.querySelector('#canvas').getContext('2d');
