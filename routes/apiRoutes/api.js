c44d4dad0ef68b0509cd1c24c3ce803571e464bd
Salome Ranson <salomeranson@gmail.com>
2021-06-20 10:17:15 -0400

    successful api call to get all workout data

--- routes/apiRoutes/api.js
+++ routes/apiRoutes/api.js
@@ -1,35 +1,51 @@
 const router = require("express").Router();
 const Workout = require("../../models/Workout");
+const mongojs = require("mongojs");
 
+const databaseUrl = "workout";
+const collections = ["workouts"];
 
+const db = mongojs(databaseUrl, collections);
+//-----------------   /api/workouts  -------------------
 
-
+// getLastWorkout()
 router.get("/workouts", (req, res) => {
-    Workout.find({})
-      .then(workoutData => {
-        res.json(workoutData);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
+  db.workouts.find({}, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.json(data);
+    }
   });
+});
 
 
 
-
+// createWorkout()   // create a new workout
 router.post("/workouts", (req, res) => {
-    console.log('Happy?');
+    console.log('Rendering exercise.html');
 })
 
 
+// addExercise()
+// router.put("/workouts/:id", ({ body }, res) => {
+//   console.log(body);
+//   db.workouts.insert(body, (error, data) => {
+//     if (error) {
+//       res.send(error);
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// })
 
 
+// getWorkoutsInRange()
 router.get("/workouts/range", (req, res) => {
 
 })
 
-router.put("/workouts/:id", (req, res) => {
-  
-})
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 const express = require("express");
+const mongojs = require("mongojs");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
-const db = require("./models");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 app.use(express.static("public"));
 
 
-db.Workout.create({ name: "Workout" })
-  .then(dbWorkouts => {
-    console.log(dbWorkouts);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
-
 // routes
 app.use(require("./routes"));
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
